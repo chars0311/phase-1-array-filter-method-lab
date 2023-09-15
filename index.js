0b9fd360709705c490a87e116918cddf85cae0e3
Charlie Salcedo <salcedo.char0311@gmail.com>
2023-09-15 00:11:15 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find drivers with an exact name match
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Function to find drivers with names that start with a given string
+  function fuzzyMatch(drivers, partialName) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(partialName.toLowerCase()));
+  }
+  
+  // Function to match drivers based on their name property
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
